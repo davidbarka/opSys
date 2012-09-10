6b5e7033282d35ccb50e82b75e94e51cd524ec06
Morten Salte <msalte86@gmail.com>
2012-09-10 11:31:09 +0200

    Task II of project 1
    Implemented the Condition2 class

--- nachos/threads/Condition2.java
+++ nachos/threads/Condition2.java
@@ -1,5 +1,6 @@
 package nachos.threads;
 
+import java.util.LinkedList;
 import nachos.machine.*;
 
 /**
@@ -12,47 +13,84 @@ import nachos.machine.*;
  * @see	nachos.threads.Condition
  */
 public class Condition2 {
-    /**
-     * Allocate a new condition variable.
-     *
-     * @param	conditionLock	the lock associated with this condition
-     *				variable. The current thread must hold this
-     *				lock whenever it uses <tt>sleep()</tt>,
-     *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
-     */
-    public Condition2(Lock conditionLock) {
-	this.conditionLock = conditionLock;
-    }
+	/**
+	 * Allocate a new condition variable.
+	 *
+	 * @param	conditionLock	the lock associated with this condition
+	 *				variable. The current thread must hold this
+	 *				lock whenever it uses <tt>sleep()</tt>,
+	 *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
+	 */
+	public Condition2(Lock conditionLock) {
+		this.conditionLock = conditionLock;
+		
+		// initializers
+		awaitingThreads = new LinkedList<KThread>();
+		interruptState = false; 
+	}
 
-    /**
-     * Atomically release the associated lock and go to sleep on this condition
-     * variable until another thread wakes it using <tt>wake()</tt>. The
-     * current thread must hold the associated lock. The thread will
-     * automatically reacquire the lock before <tt>sleep()</tt> returns.
-     */
-    public void sleep() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+	/**
+	 * Atomically release the associated lock and go to sleep on this condition
+	 * variable until another thread wakes it using <tt>wake()</tt>. The
+	 * current thread must hold the associated lock. The thread will
+	 * automatically reacquire the lock before <tt>sleep()</tt> returns.
+	 */
+	public void sleep() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
-	conditionLock.release();
+		KThread current = KThread.currentThread();
+		awaitingThreads.add(current); // add the current thread to a list of waiters
 
-	conditionLock.acquire();
-    }
+		conditionLock.release();
+		
+		interruptState = Machine.interrupt().disable();
+		
+		KThread.sleep();
+		
+		Machine.interrupt().restore(interruptState);
+		
+		// when the thread is woken up, it will try to re-acquire the lock
+		conditionLock.acquire();
+	}
 
-    /**
-     * Wake up at most one thread sleeping on this condition variable. The
-     * current thread must hold the associated lock.
-     */
-    public void wake() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-    }
+	/**
+	 * Wake up at most one thread sleeping on this condition variable. The
+	 * current thread must hold the associated lock.
+	 */
+	public void wake() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+		if(!awaitingThreads.isEmpty()) {
+			interruptState = Machine.interrupt().disable();
+			
+			// remove the first thread from the waiter queue and set it to ready
+			awaitingThreads.removeFirst().ready(); 
+			
+			Machine.interrupt().restore(interruptState);
+		}
+		
+	}
 
-    /**
-     * Wake up all threads sleeping on this condition variable. The current
-     * thread must hold the associated lock.
-     */
-    public void wakeAll() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-    }
+	/**
+	 * Wake up all threads sleeping on this condition variable. The current
+	 * thread must hold the associated lock.
+	 */
+	public void wakeAll() {
+		Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
-    private Lock conditionLock;
+		if(!awaitingThreads.isEmpty()) {
+			interruptState = Machine.interrupt().disable();
+			
+			// remove all threads from the waiter queue and set them to ready
+			for(int i=0;i<awaitingThreads.size();i++) {
+				awaitingThreads.remove(i).ready();
+			}
+			
+			Machine.interrupt().restore(interruptState);
+		}
+
+	}
+
+	private Lock conditionLock;
+	private boolean interruptState;
+	private LinkedList<KThread> awaitingThreads;
 }
